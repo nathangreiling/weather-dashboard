b1023c830df3d07e89a3602cd428696785d01978
nathangreiling <ngreiling20@gmail.com>
2022-07-06 23:12:34 -0500

    working on JS
    * user's input is saved in local storage
    * api calls to openweathermap works
    * content from api is displayed on html page
    * local storage is displayed on html page

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+var key = "603b52e788a8259f32d720b2e67f9494"
+var today = new Date().toLocaleDateString()
+
+var button = document.querySelector('.btn');
+var input = document.querySelector('.cityInput');
+var city = document.querySelector('.name');
+var wind = document.querySelector('.wind');
+var temp = document.querySelector('.temp');
+var humid = document.querySelector('.humidity');
+var historyContainer = document.querySelector('.history-container')
+
+button.addEventListener('click', function(e){
+    e.preventDefault();
+    getWeather();
+    getForecast();
+    storage();
+    
+});
+
+var getWeather = function () {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=imperial&appid=' + key)
+        .then(response => response.json())
+        .then(response => {
+      var tempValue = response.main.temp;
+      var nameValue = response.name +" "+ "(" +today+ ")";
+      var windValue = response.wind.speed;
+      var humidValue = response.main.humidity;
+    
+      city.innerHTML = nameValue;
+      wind.innerHTML = "Wind: "+windValue+" MPH";
+      temp.innerHTML = "Temp: "+tempValue+" °F";
+      humid.innerHTML = "Humidity: "+humidValue+"%";
+      input.value ="";
+    }).catch(function(error) {
+        alert("No city found with that name!")
+    });
+};
+
+var storage = function () {
+    localStorage.setItem("cityInput", input.value)
+    var history = localStorage.getItem('cityInput')
+    var searches = document.createElement("button")
+    
+    searches.className = "historybtn"
+    searches.innerHTML = history
+    historyContainer.appendChild(searches)
+}  
+
+var getForecast = function () {
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q="+input.value+",us&cnt=5&appid=" + key)
+}
